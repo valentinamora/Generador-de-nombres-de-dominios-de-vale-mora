f06607f78d0eb47fa05e85074c3a94a2204dd335
valentinamora <148815425+valentinamora@users.noreply.github.com>
2024-04-30 03:51:33 +0000

    Generador de dominios de Java

--- src/app.js
+++ src/app.js
@@ -4,8 +4,21 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import gh from "parse-github-url";
 
 window.onload = function() {
   //write your code here
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        console.log(`${pronoun}${adj}${noun}.com`);
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
